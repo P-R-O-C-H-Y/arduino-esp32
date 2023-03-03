94c555bf16edc8cac6b69ad1180fc0bebb2572cf
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-03-03 11:32:52 +0100

    add install IDE

--- .github/scripts/install-arduino-libraries.sh
+++ .github/scripts/install-arduino-libraries.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 export ARDUINO_LIB_PATH="$ARDUINO_USR_PATH/libraries"
-
-echo "List of libraries:"
-cd "$ARDUINO_LIB_PATH"
-for entry in "$ARDUINO_LIB_PATH"/*
-    do
-    echo "$entry"
-    done
-echo ""
-
 if [ ! -d "$ARDUINO_LIB_PATH" ]; then
     echo "Creating Arduino Library folder ..."
     sudo mkdir -p "$ARDUINO_LIB_PATH"
@@ -21,6 +12,12 @@ if [ ! -d "$ARDUINO_LIB_PATH" ]; then
     echo "Arduino libraries has been installed in '$ARDUINO_LIB_PATH'"
     echo "List of libraries:"
     for entry in "$ARDUINO_LIB_PATH"/*
+        do
+        echo "$entry"
+        done
+    echo ""
+
+    for entry in "$ARDUINO_LIB_PATH"/libraries/*
         do
         echo "$entry"
         done
