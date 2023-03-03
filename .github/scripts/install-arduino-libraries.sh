e36cc8679a268b1f2a643b9ce1d8c8844b61ae68
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-03-03 12:32:51 +0100

    fix typo

--- .github/scripts/install-arduino-libraries.sh
+++ .github/scripts/install-arduino-libraries.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export ARDUINO_LIB_PATH="$HOME/Arduino"/libraries"
+export ARDUINO_LIB_PATH="$HOME/Arduino/libraries"
 if [ ! -d "$ARDUINO_LIB_PATH" ]; then
     echo "$HOME/Arduino/libraries"
     echo "$ARDUINO_LIB_PATH"
-    cd "$HOME/Arduino"/libraries"
+    cd "$HOME/Arduino/libraries"
     echo "Cloning Arduino Libraries ..."
         git clone https://github.com/FastLED/FastLED.git FastLED
 
     echo "Arduino libraries has been installed in '$ARDUINO_LIB_PATH'"
     echo "List of libraries:"
-    for entry in "./"
+    for entry in "./*"
         do
         echo "$entry"
         done
