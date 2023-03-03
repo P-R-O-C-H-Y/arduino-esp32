5a6c9ac18ada5dcafb9bf00143c4edb75990c2b3
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-03-03 11:38:51 +0100

    fix folder creating

--- .github/scripts/install-arduino-libraries.sh
+++ .github/scripts/install-arduino-libraries.sh
@@ -2,22 +2,20 @@
 
 export ARDUINO_LIB_PATH="$ARDUINO_USR_PATH/libraries"
 if [ ! -d "$ARDUINO_LIB_PATH" ]; then
-    echo "Creating Arduino Library folder ..."
-    sudo mkdir -p "$ARDUINO_LIB_PATH"
-    cd "$ARDUINO_LIB_PATH"
-
+    cd "$ARDUINO_USR_PATH/libraries"
     echo "Cloning Arduino Libraries ..."
         git clone https://github.com/FastLED/FastLED.git FastLED
 
-    echo "Arduino libraries has been installed in '$ARDUINO_LIB_PATH'"
+    echo "Arduino libraries has been installed in '$ARDUINO_USR_PATH'/libraries"
     echo "List of libraries:"
-    for entry in "$ARDUINO_LIB_PATH"/*
+    for entry in "$ARDUINO_USR_PATH"/*
         do
         echo "$entry"
         done
     echo ""
-
-    for entry in "$ARDUINO_LIB_PATH"/libraries/*
+    echo "/"
+    echo ""
+    for entry in "$ARDUINO_USR_PATH"/libraries/*
         do
         echo "$entry"
         done
