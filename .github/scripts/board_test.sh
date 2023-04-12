823f010dff0b1e46983261b44696ca1a9b0adaef
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-04-12 14:42:30 +0200

    Update board_test.sh

--- .github/scripts/board_test.sh
+++ .github/scripts/board_test.sh
@@ -90,7 +90,7 @@ echo $board_count
 json_matrix='{"fqbn":['
 for board in ${boards_array[@]}
 do
-    json_matrix+='"$board"'
+    json_matrix+='"'$board'"'
     if [ $board_count -gt 1 ]
     then
         json_matrix+=","
